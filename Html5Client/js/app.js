762e77904463caa563b9f7e9fa803e01f71d8b3b
Bob den Otter <bob@twokings.nl>
2012-11-14 17:38:01 +0100

    Working on HTML5 app..

--- Html5Client/js/app.js
+++ Html5Client/js/app.js
@@ -1,116 +1,244 @@
-// Voor een handige tutorial, zie: 
-// http://return-true.com/2012/05/getting-started-with-emberjs/
 
-/**************************
-* Application
-**************************/
+/**
+ * Model for a single Kwasr event. Not to be confused with 'events' in Javascript.
+ */
+var Event = Backbone.Model.extend({
 
-App = Ember.Application.create();
+    initialize: function(){
+        _.bindAll(this, 'logo');
+    },
+
+    defaults: {
+        eve_countrycode: null,
+        eve_end_date: null,
+        eve_id: null,
+        eve_name_en: null,
+        eve_photo_url: null,
+        eve_start_date: null,
+        eve_url: null,
+        evet_description: null,
+        evet_languagecode: null,
+        evet_map_url: null,
+        evet_name: null,
+        evet_name_slug: null,
+        evet_official_website: null
+    },
+    logo: function() {
+        return 'http://kwasr.net/images/event/small-logo/' + this.get('eve_id') + '.png';
+    },
+
+});     
 
 
-/**************************
-* Models
-**************************/
-
-App.Event = Ember.Object.extend({
-	eve_countrycode: null,
-	eve_end_date: null,
-	eve_id: null,
-	eve_name_en: null,
-	eve_photo_url: null,
-	eve_start_date: null,
-	eve_url: null,
-	evet_description: null,
-	evet_languagecode: null,
-	evet_map_url: null,
-	evet_name: null,
-	evet_name_slug: null,
-	evet_official_website: null,
-	logo: function() {
-	    return 'http://kwasr.net/images/event/small-logo/' + this.get('eve_id') + '.png';
-	}.property()	
+/**
+ * Collection for multiple Kwasr events. Not to be confused with 'events' in Javascript.
+ */
+var Eventlist = Backbone.Collection.extend({
+    model: Event
 });
 
-App.Location = Ember.Object.extend({
-	loc_id: null,
-	loc_radius_meters: null,
-	loct_description: null,
-	loc_name_en: null,
-	loc_latitude: null,
-	loc_longitude: null,
-	eve_id: null,
-	loct_languagecode: null,
-	loct_name_slug: null,
-	loct_name: null,
-	loc_photo_url: null
+
+
+
+var Location = Backbone.Model.extend({
+    defaults: {
+        loc_id: null,
+        loc_radius_meters: null,
+        loct_description: null,
+        loc_name_en: null,
+        loc_latitude: null,
+        loc_longitude: null,
+        eve_id: null,
+        loct_languagecode: null,
+        loct_name_slug: null,
+        loct_name: null,
+        loc_photo_url: null
+    }
 });
 
-App.Feature = Ember.Object.extend({
-	loc_id: null,
-	feat_name_slug: null,
-	feat_name: null,
-	fea_id: null,
-	fea_photo_url: null,
-	sho_nr_posts: null,
-	fea_imdb_id: null,
-	fea_nr_posts: null,
-	sho_id: null,
-	fea_name_en: null,
-	sho_start_datetime: null,
-	sho_avg_post_rating: null,
-	sho_end_datetime: null,
-	fea_lastfm_id: null,
-	feat_description: null,
-	feat_languagecode: null,
-	fea_avg_post_rating: null,
-	image: function() {
-	    // return 'http://kwasr.net/images/event/small-logo/' + this.get('eve_id') + '.png';
-	    return this.get('fea_photo_url');
-	}.property()	
+var Locationlist = Backbone.Collection.extend({
+    model: Location
 });
 
+
+var Feature = Backbone.Model.extend({
+    defaults: {
+        loc_id: null,
+        feat_name_slug: null,
+        feat_name: null,
+        fea_id: null,
+        fea_photo_url: null,
+        sho_nr_posts: null,
+        fea_imdb_id: null,
+        fea_nr_posts: null,
+        sho_id: null,
+        fea_name_en: null,
+        sho_start_datetime: null,
+        sho_avg_post_rating: null,
+        sho_end_datetime: null,
+        fea_lastfm_id: null,
+        feat_description: null,
+        feat_languagecode: null,
+        fea_avg_post_rating: null
+    }, 
+    image: function() {
+        // return 'http://kwasr.net/images/event/small-logo/' + this.get('eve_id') + '.png';
+        return this.get('fea_photo_url');
+    }    
+});
+
+var Featurelist = Backbone.Collection.extend({
+    model: Feature
+});
+
+
+/**
+ * "View" controller for Events
+ */
+var EventView = Backbone.View.extend({
+
+    initialize: function() {
+        this.collection = new Eventlist();
+
+        // Als 'add' van de collectie wordt aangeroepen, doen wij renderList..
+        //this.collection.bind('add', this.renderList);
+
+    },
+
+    load: function() {
+        var url = "http://api.kwasr.net/v1/nl/events";
+        $.ajax({
+            url: url,
+            dataType: 'JSONP',
+            context: this,
+            success: this.loadSucceeded,
+            completed: this.loadCompleted
+        });
+    },
+
+    loadSucceeded: function(data) {
+
+        data.events.forEach(function(eventdata){
+
+            var event = new Event();
+            event.set(eventdata);
+
+            this.collection.add(event);
+
+        }, this);
+
+        this.render();
+
+    },
+
+    loadCompleted: function() {
+        if(xhr.status == 400 || xhr.status == 420) {
+            alert("API limit reached.");
+        }
+    },
+
+    render: function() {
+        var template = Handlebars.compile($("#eventlisttemplate").html());
+        var html = template({ events: this.collection.toJSON() });
+        console.log($(this.el));
+        $('#eventlistview').html(html);
+        return this;
+    }
+
+
+});
+
+
+var eventView = new EventView();
+
+
+
+var FeatureView = Backbone.View.extend({
+
+    initialize: function() {
+        this.collection = new Featurelist();
+
+        // Als 'add' van de collectie wordt aangeroepen, doen wij renderList..
+        //this.collection.bind('add', this.renderList);
+
+    },
+
+    load: function(id) {
+        var url = "http://api.kwasr.net/v1/nl/event/" + id;
+        $.ajax({
+            url: url,
+            dataType: 'JSONP',
+            context: this,
+            success: this.loadSucceeded,
+            completed: this.loadCompleted
+        });
+    },
+
+    loadSucceeded: function(data) {
+
+        console.log(data);
+
+        data.events.forEach(function(eventdata){
+
+            var event = new Event();
+            event.set(eventdata);
+
+            this.collection.add(event);
+
+        }, this);
+
+        this.render();
+
+    },
+
+    loadCompleted: function() {
+        if(xhr.status == 400 || xhr.status == 420) {
+            alert("API limit reached.");
+        }
+    },
+
+    render: function() {
+        var template = Handlebars.compile($("#eventlisttemplate").html());
+        var html = template({ events: this.collection.toJSON() });
+        console.log($(this.el));
+        $('#eventlistview').html(html);
+        return this;
+    }
+
+
+});
+
+
+var featureView = new FeatureView();
+
+
+
+Handlebars.registerHelper('logo', function(object) {
+  return 'http://kwasr.net/images/event/small-logo/' + object.eve_id + '.png';
+});
+
+/*
+
+
+*/
 
 /**************************
 * Views
 **************************/
 
+/*
 App.eventV = Ember.View.extend(Ember.Metamorph, {
 
 });
+*/
 
 
 /**************************
 * Controllers
 **************************/
 
-App.eventsC = Ember.ArrayController.create({
-    content: [],
-    loadEvents: function() {
-    	var url = "http://api.kwasr.net/v1/nl/events";
-		$.ajax({
-			url: url,
-			dataType: 'JSONP',
-			context: this,
-			success: this.loadEventsSucceeded,
-			completed: this.loadEventsCompleted
-		});
-    },
-    loadEventsSucceeded: function(data) {
-		this.set('content', []);
-		var events = data.events;
-		events.forEach(function(event){
-			var e = App.Event.create(event);
-			this.pushObject(e);
-		}, this);
-    },
-    loadEventsCompleted: function() {
-    	if(xhr.status == 400 || xhr.status == 420) {
-    		alert("API limit reached.");
-     	}
 
-    }
-});
-
+/*
 
 App.performanceC = Ember.ArrayController.create({
     content: [],
@@ -141,7 +269,11 @@ App.performanceC = Ember.ArrayController.create({
     		alert("API limit reached.");
      	}
 
-    }
+    },
+    loadPerformance: function(id) {
+
+
+    },    
 });
 
 
@@ -177,3 +309,4 @@ App.locationsC = Ember.ArrayController.create({
     }
 });
 
+*/
--- Html5Client/js/frontend.js
+++ Html5Client/js/frontend.js
@@ -4,26 +4,61 @@ jQuery(function( $ ){
 
 	$('nav#main a').bind('click', function(e){
 		// e.preventDefault();
-		// console.log('klik', $(this).attr('href'));
+		console.log('klik 1', $(this).attr('href'));
 		$('body').scrollTo($( $(this).attr('href') ), 300);
 	});
 
 	$('#devoptions').bind('change', function(e){
 		// e.preventDefault();
-		console.log('klik', $(this).attr('value'));
+		console.log('klik 2', $(this).attr('value'));
 		$('body').scrollTo($("#" + $(this).attr('value') ), 300);
 	});
+
+
+	// Helper to make things like '<button data-action="eventView.load">' work
+	$('button, h2 a').live('click', function(e){
+
+		var action = $(this).data('action');
+
+		// console.log('action', action);
+
+		if (action != "") {
+			eval(action);
+		}
+
+	});
+
+
+
+
+/*
+
 
 
 	// Links in headers in #list-events
 	$('#list-events h2 a').live('click', function(){
 		id = $(this).data('id');
-		console.log('id:', id);
-		
 		App.performanceC.loadPerformances(id);
 		App.locationsC.loadLocations(id);
-
 	});
+
+	// Links in headers in #list-performances
+	$('#list-performances h2 a').live('click', function(){
+		id = $(this).data('id');
+		console.log('id: ', id)
+		// App.performanceC.loadPerformance(id);
+	});
+*/
 
 
 });
+
+
+function eventclick(id) {
+
+
+	console.log('id', id);
+
+	featureView.load(id);
+
+}
